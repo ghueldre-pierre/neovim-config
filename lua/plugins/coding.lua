69c9b9bbc660d9bec61ecec43ff82c5e36e8ba25
Pierre Ghueldre <ghueldre.pierre@laposte.net>
2025-09-15 09:49:22 +0200

    add nvim-ts-autotag - remove nvim-autopairs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,11 +1,20 @@
 return {
   {
+    "windwp/nvim-ts-autotag",
+    config = true
+  },
+  --[[
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = true
   },
+  --]]
+  --[[
   {
-    "windwp/nvim-ts-autotag",
-    config = true
-  }
+    -- https://github.com/mattn/emmet-vim ?
+    -- Avec VSCode, j'utilisais emmet pour créer des fichiers HTML de base
+    -- je préfère éviter de l'installer car je souhaite ne pas avoir trop de plugins
+  },
+  --]]
 }
